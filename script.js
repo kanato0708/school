094ea3da7c618d27d39e57992af2cdb3d8f3467b
Kanato <101640864+kanato0708@users.noreply.github.com>
2023-09-13 20:10:57 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const q1taxt = ['嵐のリーダーは？'];
+const q1answer = ['大野智'||'大野くん'];
+
+function EEE(){
+
+if (FFF = AAA.CCC.value == q1answer) {
+  alert("正解") ;
+
+}else{
+    
+  alert("不正解");
+}
+
+}
